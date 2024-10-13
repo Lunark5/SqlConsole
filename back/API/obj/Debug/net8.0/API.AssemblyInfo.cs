fe70511343789da1deb7479f8dcafc2f8bae758a
Даниил Баканов <bakanov.pip@gmail.com>
2024-10-13 21:18:09 +0400

    Gitignore
    Gitignore

--- back/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ back/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4c34cbb1cef0095b9e91e840909b9c2d4ebb66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da4bc724b11d042c1fbe04f9240e59b1ee24643")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
